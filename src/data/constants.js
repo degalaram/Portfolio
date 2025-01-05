a377eb4499250439e6b664f273b08a2bc08ba2fc
Ram <136690243+degalaram@users.noreply.github.com>
2025-01-05 17:17:22 +0530

    Add files via upload

---
+++ src/data/constants.js
@@ -0,0 +1,219 @@
+export const Bio = {
+  name: "Ram Degala",
+  roles: [
+    "Full Stack Developer",
+    "Programmer",
+  ],
+  description:
+    "I'm a highly motivated and versatile individual with a strong aptitude for problem-solving and a deep commitment to continuous development. I enjoy tackling complex challenges, analyzing data, and developing effective solutions. I'm a firm believer in lifelong learning and actively pursue opportunities to expand my knowledge and skillset.",
+  github: "https://github.com/degalaram",
+
+  gmail: "ramdegala9@gmail.com",
+
+  resume:
+    "https://drive.google.com/file/d/1QPN7ns0XUSLrKY_0d8Q53kYFafnwXTIR/view?usp=sharing",
+
+  linkedin: "https://www.linkedin.com/in/ramdegala/",
+
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image: "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      
+     
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image: "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      
+   
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Python",
+        image: "https://ik.imagekit.io/uko16m2rv/Screenshot%202024-12-30%20101750.png",
+      },
+      
+      {
+        name: "MySQL",
+        image:"https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+
+      {
+        name: "MongoDB",
+        image:"https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+     
+    ],
+  },
+
+  {
+    title: "Others",
+    skills: [
+    
+      {
+        name: "Git",
+        image:"https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+      },
+      {
+        name: "GitHub",
+        image:"https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Netlify",
+        image:"https://images.seeklogo.com/logo-png/27/1/netlify-logo-png_seeklogo-273744.png?v=1957226392073033288",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 5,
+    img: "https://ik.imagekit.io/uko16m2rv/Screenshot%202024-12-30%20085220.png",
+    role:"Full Stack Developer" ,
+    company: "RJ Global Solutions",
+    date: "June 2024 - December 2024",
+    desc: "As a passionate Software developer,I have hands-on experience with React and .NET, specializing in building dynamic, responsive web applications.With Good Understanding of Html,Css,Javascript and React.I focus on developing interactive, user-friendly frontends, My skillset includes working with ASP.NET Core, Entity Frameworks, and SQL database, enabling me to deliver full-stack solutions that meet business objectives.",
+  
+    skills: [
+     "React Js",
+     "Node JS",
+     "SQL",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Bootstrap"
+    ],
+   
+  },
+  {
+    id: 0,
+    img: "https://ik.imagekit.io/uko16m2rv/Screenshot%202024-12-30%20083506.png",
+    role: "Web Developer",
+    company: "Prodigy Infotech",
+    date: "July 2023 - Aug 2023",
+    desc: "Started new Position as a Web Developer intern at Prodigy InfoTech on a top skilled development platform with lot of Good Carrier Opportunities.Here, My role to create new Responsive Static Web Pages and layout structures by using technologies mentioned on my skills.",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Bootstrap"
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://ik.imagekit.io/uko16m2rv/Screenshot%202024-12-30%20102333.png",
+    school: "GATE Institue of Technology & Management Sciences",
+    date: "July 2023 - July 2025",
+    grade: "7.14 CGPA",
+    desc: "I have completed 2 years and have a CGPA of 7.14 . I have taken courses in MERN Stack, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+    degree: "Post Graduate - MCA,Master of Computer Applicaton",
+  },
+  {
+  id: 1,
+  img: "https://ik.imagekit.io/uko16m2rv/WhatsApp%20Image%202024-12-30%20at%2010.37.43_54c9f479.jpg",
+  school: "Sri Venkateswara University",
+  date: "June 2018 - October 2021",
+  grade: "7.40 CGPA",
+  desc: "I have completed 3 years of my Graduaton and have a CGPA of 7.40. I have taken courses in Object-Oriented Programming, Operating Systems, and Computer Networks, among others.",
+  degree: "Under Graduate - Degree,Bachelor of Computer Science",
+},
+{
+  id: 0,
+  img: "https://ik.imagekit.io/uko16m2rv/Screenshot%202024-12-30%20104818.png?updatedAt=1735535926835",
+  school: "Sri Gudibandi somereddy Junior College",
+  date: "July 2016 - March 2018",
+  grade: "8.32 CGPA",
+  desc: "I have completed 2 years of Intermediate and have a CGPA of 8.32. I have taken courses in Mathematics,Physics and Chemistry among others in my Intermediate.",
+  degree: "Intermediate - MPC",
+},
+];
+
+export const projects = [
+  {
+    id: 11,
+    title: "Food Search ",
+    description:
+      "Build a Food Search Restarent Page where User can Find Different varities of Non Veg menu items",
+    image:
+      "https://ik.imagekit.io/uko16m2rv/Screenshot%202024-12-30%20091308.png",
+    tags: [
+      "Html",
+      "CSS",
+      "Bootstrap",
+      "Javascript",
+    
+    ],
+    category: "web app",
+    github: "https://github.com/anshita005/Job-Portal",
+    webapp: "https://landinggg.ccbp.tech/",
+  },
+  {
+    id: 12,
+    title: "My Portfolio",
+    description:
+      "My Portfolio is a React.js application that serves as an online portfolio to showcase my work, skills, and achievements. It provides an overview of my background, displays my projects, and offers a way to contact me.",
+    image:
+      "https://ik.imagekit.io/uko16m2rv/Screenshot%202024-12-30%20100324.png",
+    tags: [
+      "React Js",
+      "HTML5",
+      "Tailwind CSS",
+      "Javascript",
+     
+    ],
+    category: "web app",
+    github: "https://github.com/anshita005/My-Portfolio",
+    webapp: "",
+  },
+  {
+    id: 9,
+    title: "Todo-List",
+    description:"A task management solution,designed to make life easier which includes daily activities.Streamlined task management through a combination of HTML,CSS,and Bootstrap for an intuitive interface.Seamless CRUD operations through JavaScript event listeners and dynamic UI updates.",
+    image: "https://ik.imagekit.io/uko16m2rv/Screenshot%202024-12-30%20100642.png",
+    tags: [
+     "Html",
+     "CSS",
+      "Javascript",
+    ],
+    category: "web app",
+    github: "https://github.com/anshita005/SpendWise",
+    webapp: "https://todoofmine.ccbp.tech/",
+  },
+];
+
